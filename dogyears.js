e46cd2f40c5ab2d43832d64978f2aed732e46a09
Daric Renova <daric.e.renova@gmail.com>
2022-04-14 20:17:28 -0600

    making a dog years small project

---
+++ dogyears.js
@@ -0,0 +1,14 @@
+let myAge = 22;
+console.log(myAge)
+let earlyYears = 2;
+console.log(earlyYears)
+earlyYears *= 10.5;
+console.log(earlyYears)
+let laterYears = myAge - 2;
+console.log(laterYears)
+laterYears *= 4
+console.log(laterYears)
+const myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears)
+let myName = 'Bob'.toLowerCase()
+console.log(`my mane is ${myName}. I am ${myAge} old in human years which is ${myAgeInDogYears} old in dog years`)
